b0b6ace1f62ddce383417feacc47eccd57ea91f3
hammad-air <hammadn788@gmail.com>
2022-12-22 00:20:12 +0500

    API-KEY

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
-openai.api_key = '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'
+openai.api_key = 'API-KEY'
 
 def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0,
                     stop=['DALIA:', 'USER:']):
