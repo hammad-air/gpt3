793eb4fc5b2b52607e0ff59e828255a91c515658
Hammad Sheikh-Bin-Nadeem <64545966+hammad-air@users.noreply.github.com>
2022-12-22 02:43:44 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ def open_file(filepath):
         return infile.read()
 
 
-openai.api_key = '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'
+openai.api_key = 'API-key'
 
 
 def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0,stop=['THEMISAPP:', 'USER:']):
